2b17959714ce88a2bd819281d621a79b6649c273
MoraisEng <comprasmoraisengenharia@gmail.com>
2025-05-09 15:38:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+function acessarArea(areaId, senha) {
+  const input = prompt(`Digite a senha para acessar a ${areaId === 'interna' ? 'ÁREA INTERNA' : 'ÁREA DOS CORRETORES'}:`);
+  if (input === senha) {
+    document.getElementById('areaInterna').style.display = (areaId === 'interna') ? 'block' : 'none';
+    document.getElementById('areaCorretores').style.display = (areaId === 'corretores') ? 'block' : 'none';
+  } else {
+    alert('Senha incorreta!');
+  }
+}
+
 function acessarMapa(nomeMapa, senha, urlMapa) {
-    let senhaInformada = prompt(`Digite a senha para acessar ${nomeMapa}:`);
-    if (senhaInformada === senha) {
-        window.open(urlMapa, '_blank');
-    } else {
-        alert('Senha incorreta!');
-    }
+  const senhaInformada = prompt(`Digite a senha para acessar ${nomeMapa}:`);
+  if (senhaInformada === senha) {
+    window.open(urlMapa, '_blank');
+  } else {
+    alert('Senha incorreta!');
+  }
 }
