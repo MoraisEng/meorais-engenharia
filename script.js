c5e6aa426c96347c9b88672a4dce15a06b70c67f
MoraisEng <comprasmoraisengenharia@gmail.com>
2025-05-21 16:05:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-function entrarArea(pagina, senha) {
-  const input = prompt('Digite a senha:');
-  if (input === senha) {
-    window.location.href = pagina;
-  } else {
-    alert('Senha incorreta!');
-  }
-}
-
-function acessarMapa(nomeMapa, senha, url) {
-  const s = prompt(`Senha para ${nomeMapa}:`);
-  if (s === senha) window.open(url, '_blank');
-  else alert('Senha incorreta!');
+function mostrarSecao(id) {
+  // esconde todas
+  document.querySelectorAll('section.area').forEach(sec => sec.style.display = 'none');
+  // mostra a desejada
+  document.getElementById(id).style.display = 'block';
 }
